02108babfac2441ef1c958d114625e84773ac8d6
Hasan9955 <hasanbinali7556@gmail.com>
2023-09-16 06:19:15 +0600

    make-course-card

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
+import { useEffect, useState } from "react"
+import Courses from "./components/Courses/Courses"
 
 
 
 function App() {
+  const [courses, setCourses] = useState([])
+
+  useEffect(()=> {
+    fetch('data.json')
+    .then(res => res.json())
+    .then(data => setCourses(data))
+  },[])
 
   return (
     <>
       
-      <h1 className="text-4xl font-bold bg-slate-100">Registration</h1>
+      <header className="text-4xl font-bold text-center m-10">Course Registration </header>
+      <div className="md:m-10 p-5">
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-5 md:w-3/4">
+            {
+              courses.map(course =>(  
+              <Courses
+              key={course.id}
+              course={course}
+              ></Courses>))
+            }
+        </div>
+        <div>
+
+        </div>
+      </div>
       
     </>
   )
---
+++ src/components/Courses/Courses.jsx
@@ -0,0 +1,26 @@
+import { FaDollarSign } from 'react-icons/fa';
+import { BsBook } from 'react-icons/bs';
+
+const Courses = ({course}) => {
+    console.log(course)
+    const {id, title, cover_img, description, price, credit} = course;
+    return (
+        <div className="space-y-5 border-2 p-3 rounded-xl">
+            <img src={cover_img} alt="" />
+            <h1 className='text-lg font-[600]'>{title}</h1>
+            <p className='font-[400] text-[#1C1B1B99]'>{description}</p>
+            <div className='flex items-center gap-3'>
+                <FaDollarSign></FaDollarSign>
+                <p>Price: {price}</p>
+
+                <BsBook></BsBook>
+                <p>Credit: {credit}hr</p>
+
+            </div>
+            <button className="text-white font-bold
+             bg-[#2F80ED] p-3 rounded-xl w-full">Select</button>
+        </div>
+    );
+};
+
+export default Courses;
